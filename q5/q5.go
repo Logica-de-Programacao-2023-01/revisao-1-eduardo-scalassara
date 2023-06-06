6c5c062062e9366c95e8a5c6feedeccf5e56bfa5
eduardoscal <126472349+eduardoscal@users.noreply.github.com>
2023-06-06 16:28:41 -0300

    Update q5.go

--- q5/q5.go
+++ q5/q5.go
@@ -1,6 +1,38 @@
 package q5
 
+import "fmt"
+
 func ConvertTemperature(temp float64, fromScale string, toScale string) (float64, error) {
-	// Implemente sua solução aqui
-	return 0, nil
+	switch fromScale {
+	case "C":
+		switch toScale {
+		case "F":
+			return (temp * 9/5) + 32, nil
+		case "K":
+			return temp + 273.15, nil
+		default:
+			return 0, fmt.Errorf("escala inválida")
+		}
+	case "F":
+		switch toScale {
+		case "C":
+			return (temp - 32) * 5/9, nil
+		case "K":
+			return (temp - 32) * 5/9 + 273.15, nil
+		default:
+			return 0, fmt.Errorf("escala inválida")
+		}
+	case "K":
+		switch toScale {
+		case "C":
+			return temp - 273.15, nil
+		case "F":
+			return (temp - 273.15) * 9/5 + 32, nil
+		default:
+			return 0, fmt.Errorf("escala inválida")
+		}
+	default:
+		return 0, fmt.Errorf("escala inválida")
+	}
 }
+
